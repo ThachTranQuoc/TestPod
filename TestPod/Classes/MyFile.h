d5c605f73578d138a2a93739b113cce060d0cc4a
Thach Tran Quoc <quocthachah11@yahoo.com>
2016-05-09 22:35:23 +0700

    [ADD]Add File

---
+++ TestPod/Classes/MyFile.h
@@ -0,0 +1,13 @@
+//
+//  MyFile.h
+//  Pods
+//
+//  Created by Thach Tran Quoc on 5/9/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyFile : NSObject
+
+@end
---
+++ TestPod/Classes/MyFile.m
@@ -0,0 +1,13 @@
+//
+//  MyFile.m
+//  Pods
+//
+//  Created by Thach Tran Quoc on 5/9/16.
+//
+//
+
+#import "MyFile.h"
+
+@implementation MyFile
+
+@end
